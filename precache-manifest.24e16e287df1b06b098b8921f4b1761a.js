17c485eb37b800537c2ab6cc8f16a442906d81fd
Rohan <thakur190420@gmail.com>
2020-08-05 00:57:57 +0530

    Updates

---
+++ precache-manifest.24e16e287df1b06b098b8921f4b1761a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051a2cc0ce91bec1d735d4917b3023e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5361fe73a6b718175848",
+    "url": "/static/css/main.95bc065a.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5361fe73a6b718175848",
+    "url": "/static/js/main.e6dccbfb.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/font.12a3db8b.woff"
+  }
+]);
