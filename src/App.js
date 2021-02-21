b1daaae0c9582094eff888d2e459f72f78427d6a
xebec19 <thakur190420@gmail.com>
2021-02-21 14:22:30 +0530

    save

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState,useEffect } from 'react';
 import Cardlist from './components/Cardlist';
 import Searchbox from './components/Searchbox'
 import {robots} from './components/robots';
@@ -6,26 +6,35 @@ import Scroll from './components/Scroll';
 import ErrorBoundry from './components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-	constructor() {
+function App() {
+	/*constructor() {
 		super()
 		this.state = {
 	robots: robots,
 	searchfield: ''
 	}
-}
-	componentDidMount(){
+}*/
+const [robots, setRobots] = useState([])
+const [searchfield, setSearchField] = useState('')
+
+useEffect(() => {
+	fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then((users) => {
+			setRobots(users);
+			console.log(users);
+	})	
+},[])
+	/*componentDidMount(){
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
 		.then(users => {this.setState({robots:users})});	
+}*/
+const onSearchChange = (event) => {
+	/*this.setState({ searchfield: event.target.value })*/
+	setSearchField(event.target.value);
 }
-onSearchChange = (event) => {
-	this.setState({ searchfield: event.target.value })
-}
-	
-	render(){
-		const{ robots,searchfield } = this.state;
-		const filteredRobots = robots.filter(robots =>{
+	const filteredRobots = robots.filter((robots) => {
 		return robots.name.toLowerCase().includes(searchfield.toLowerCase());
 	})
 	if(!robots.length){
@@ -34,7 +43,7 @@ onSearchChange = (event) => {
 		return (
 		<div className='tc'>
 		<h1 className='f1'>Robofriends</h1>
-		<Searchbox searchChange={this.onSearchChange}/>
+		<Searchbox searchChange={onSearchChange}/>
 		<Scroll>
 		<ErrorBoundry>
 		<Cardlist robots={filteredRobots}/>
@@ -43,6 +52,6 @@ onSearchChange = (event) => {
 		</div>
 		);
 	}
-}
+/*}*/
 }
 export default App;
